25da777d8cb8e6a4dd35fca51fd00c7265de3ef8
faruksertbay <108140539+faruksertbay@users.noreply.github.com>
2022-08-16 16:09:41 +0300

    Add files via upload

--- AuthController.cs
+++ AuthController.cs
@@ -2,16 +2,38 @@
 
 namespace _archive.Controllers
 {
+    
     public class AuthController : Controller
     {
         public IActionResult Login()
         {
             return View();
         }
+        
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username != null && username.Equals("acc1") && password.Equals("123"))
+            {
+                HttpContext.Session.SetString("username", username);
+                return View("Home/Index");
+            }
+            else
+            {
+                ViewBag.error = "Invalid Account";
+                return View("Index");
+            }
+        }
 
+
+        [Route("logout")]
+        [HttpGet]
         public IActionResult Logout()
         {
-            return View();
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
         }
     }
+
+
 }
