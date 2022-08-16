990fc9d9a29a8b4e3c3c99ec1f6302113e67f9ae
faruksertbay <108140539+faruksertbay@users.noreply.github.com>
2022-08-16 16:06:40 +0300

    Add files via upload

---
+++ AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _archive.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
